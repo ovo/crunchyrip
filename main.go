56edc31497f3ab6258c0a9644e5aa832bf80d30a
ovo <peyton.arbuckle@gmail.com>
2021-03-05 11:11:16 -0600

    ✨ added resolution subcommand

--- main.go
+++ main.go
@@ -18,142 +18,73 @@ import (
 func main() {
 	app := cli.App{
 		Name:  "crunchyrip",
-		Usage: "download crunchyroll episodes",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "email",
-				Value:    "",
-				Usage:    "email for crunchyroll account",
-				Required: true,
+		Usage: "download full crunchyroll episodes",
+		Commands: []*cli.Command{
+			{
+				Name:    "download",
+				Aliases: []string{"d"},
+				Usage:   "download episodes or a season",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "email",
+						Value:    "",
+						Usage:    "email for crunchyroll account",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "password",
+						Value:    "",
+						Usage:    "password for the crunchyroll account",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:  "episodeIDs",
+						Value: "",
+						Usage: "comma-separated episode IDs; no comma needed for single episode",
+					},
+					&cli.StringFlag{
+						Name:  "locale",
+						Value: "en-US",
+					},
+					&cli.StringFlag{
+						Name:  "resolution",
+						Value: "",
+						Usage: "resolution of the download (ex. 1080x1920) - defaults to highest resolution",
+					},
+					&cli.StringFlag{
+						Name:  "seriesID",
+						Value: "",
+						Usage: "ID of the series you want to download a season for",
+					},
+				},
+				Action: downloadAction,
 			},
-			&cli.StringFlag{
-				Name:     "password",
-				Value:    "",
-				Usage:    "password for the crunchyroll account",
-				Required: true,
+			{
+				Name:    "resolution",
+				Aliases: []string{"r"},
+				Usage:   "get resolutions for an episode",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "email",
+						Value:    "",
+						Usage:    "email for crunchyroll account",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "password",
+						Value:    "",
+						Usage:    "password for the crunchyroll account",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "episodeID",
+						Value:    "",
+						Usage:    "episodeID to get resolutions for",
+						Required: true,
+					},
+				},
+				Action: resolutionAction,
 			},
-			&cli.StringFlag{
-				Name:  "episodeIDs",
-				Value: "",
-				Usage: "comma-separated episode ID on vrv.co ex. https://vrv.co/watch/ -> GRMGEZ85R <- /Hunter-x-Hunter",
-			},
-			&cli.StringFlag{
-				Name:  "locale",
-				Value: "en-US",
-			},
-			&cli.StringFlag{
-				Name:  "resolution",
-				Value: "",
-				Usage: "resolution of the download",
-			},
-			&cli.StringFlag{
-				Name:  "seriesID",
-				Value: "",
-				Usage: "ID of the series you want to download a season for",
-			},
-		},
-		Action: func(c *cli.Context) error {
-			var wg sync.WaitGroup
-			tr := &http.Transport{
-				MaxIdleConns:        20,
-				MaxIdleConnsPerHost: 20,
-			}
-			client := http.Client{Transport: tr}
-			client.Jar, _ = cookiejar.New(nil)
-
-			log.Println("Logging in")
-			credentials, err := cr.Login(&client, c.String("email"), c.String("password"))
-
-			if err != nil {
-				return err
-			}
-
-			log.Println("Getting CMS info")
-			cms, err := cr.GetCMS(&client, credentials.AccessToken)
-
-			authConfig := cr.AuthConfig{
-				AccessToken: credentials.AccessToken,
-				Policy:      cms.Cms.Policy,
-				Signature:   cms.Cms.Signature,
-				KeyPairID:   cms.Cms.KeyPairID,
-				Bucket:      cms.Cms.Bucket,
-			}
-
-			if c.String("episodeIDs") != "" {
-				ids := strings.Split(c.String("episodeIDs"), ",")
-
-				wg.Add(len(ids))
-
-				for _, id := range ids {
-					go func(c *http.Client, authConfig cr.AuthConfig, id string, resolution string, locale string) {
-						log.Println("Getting episode info for " + id)
-
-						episode, err := cr.GetEpisode(c, authConfig, id)
-
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						streamURL, err := cr.GetStreamURL(c, authConfig, episode.Links.Streams.Href, locale)
-
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						log.Println("Downloading " + episode.ID)
-						go cr.DownloadStream(c, authConfig, streamURL, resolution, episode, &wg)
-
-					}(&client, authConfig, id, c.String("resolution"), c.String("locale"))
-
-				}
-			}
-
-			if c.String("seriesID") != "" {
-				seasons, err := cr.GetSeasons(&client, authConfig, c.String("seriesID"))
-
-				if err != nil {
-					return err
-				}
-
-				for i, s := range seasons {
-					fmt.Println(i+1, s.Title)
-				}
-
-				reader := bufio.NewReader(os.Stdin)
-				fmt.Print("Enter season to download: ")
-				text, _ := reader.ReadString('\n')
-				index, err := strconv.Atoi(strings.Trim(text, "\n"))
-
-				if err != nil {
-					return err
-				}
-
-				log.Println("Getting episode info for " + seasons[index-1].Title)
-				episodes, err := cr.GetEpisodes(&client, authConfig, seasons[index-1].ID)
-
-				if err != nil {
-					return err
-				}
-
-				wg.Add(len(episodes))
-
-				for _, e := range episodes {
-					go func(c *http.Client, authConfig cr.AuthConfig, ep cr.Episode, resolution string, locale string) {
-						streamURL, err := cr.GetStreamURL(c, authConfig, ep.Links.Streams.Href, locale)
-
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						log.Println("Downloading " + ep.ID)
-						go cr.DownloadStream(c, authConfig, streamURL, resolution, ep, &wg)
-					}(&client, authConfig, e, c.String("resolution"), c.String("locale"))
-				}
-			}
-
-			wg.Wait()
-
-			return nil
 		},
 	}
 
@@ -163,4 +94,159 @@ func main() {
 		log.Fatal(err)
 	}
 
+}
+
+func downloadAction(c *cli.Context) error {
+	var wg sync.WaitGroup
+	tr := &http.Transport{
+		MaxIdleConns:        20,
+		MaxIdleConnsPerHost: 20,
+	}
+	client := http.Client{Transport: tr}
+	client.Jar, _ = cookiejar.New(nil)
+
+	log.Println("Logging in")
+	credentials, err := cr.Login(&client, c.String("email"), c.String("password"))
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("Getting CMS info")
+	cms, err := cr.GetCMS(&client, credentials.AccessToken)
+
+	authConfig := cr.AuthConfig{
+		AccessToken: credentials.AccessToken,
+		Policy:      cms.Cms.Policy,
+		Signature:   cms.Cms.Signature,
+		KeyPairID:   cms.Cms.KeyPairID,
+		Bucket:      cms.Cms.Bucket,
+	}
+
+	if c.String("episodeIDs") != "" {
+		ids := strings.Split(c.String("episodeIDs"), ",")
+
+		wg.Add(len(ids))
+
+		for _, id := range ids {
+			go func(c *http.Client, authConfig cr.AuthConfig, id string, resolution string, locale string) {
+				log.Println("Getting episode info for " + id)
+
+				episode, err := cr.GetEpisode(c, authConfig, id)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				streamURL, err := cr.GetStreamURL(c, authConfig, episode.Links.Streams.Href, locale)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Println("Downloading " + episode.ID)
+				go cr.DownloadStream(c, authConfig, streamURL, resolution, episode, &wg)
+
+			}(&client, authConfig, id, c.String("resolution"), c.String("locale"))
+
+		}
+	}
+
+	if c.String("seriesID") != "" {
+		seasons, err := cr.GetSeasons(&client, authConfig, c.String("seriesID"))
+
+		if err != nil {
+			return err
+		}
+
+		for i, s := range seasons {
+			fmt.Println(i+1, s.Title)
+		}
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter season to download: ")
+		text, _ := reader.ReadString('\n')
+		index, err := strconv.Atoi(strings.Trim(text, "\n"))
+
+		if err != nil {
+			return err
+		}
+
+		log.Println("Getting episode info for " + seasons[index-1].Title)
+		episodes, err := cr.GetEpisodes(&client, authConfig, seasons[index-1].ID)
+
+		if err != nil {
+			return err
+		}
+
+		wg.Add(len(episodes))
+
+		for _, e := range episodes {
+			go func(c *http.Client, authConfig cr.AuthConfig, ep cr.Episode, resolution string, locale string) {
+				streamURL, err := cr.GetStreamURL(c, authConfig, ep.Links.Streams.Href, locale)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Println("Downloading " + ep.ID)
+				go cr.DownloadStream(c, authConfig, streamURL, resolution, ep, &wg)
+			}(&client, authConfig, e, c.String("resolution"), c.String("locale"))
+		}
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
+func resolutionAction(c *cli.Context) error {
+	tr := &http.Transport{
+		MaxIdleConns:        20,
+		MaxIdleConnsPerHost: 20,
+	}
+	id := c.String("episodeID")
+	client := http.Client{Transport: tr}
+	client.Jar, _ = cookiejar.New(nil)
+
+	log.Println("Logging in")
+	credentials, err := cr.Login(&client, c.String("email"), c.String("password"))
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("Getting CMS info")
+	cms, err := cr.GetCMS(&client, credentials.AccessToken)
+
+	authConfig := cr.AuthConfig{
+		AccessToken: credentials.AccessToken,
+		Policy:      cms.Cms.Policy,
+		Signature:   cms.Cms.Signature,
+		KeyPairID:   cms.Cms.KeyPairID,
+		Bucket:      cms.Cms.Bucket,
+	}
+
+	log.Println("Getting episode info for " + id)
+
+	episode, err := cr.GetEpisode(&client, authConfig, id)
+
+	if err != nil {
+		return err
+	}
+
+	streamURL, err := cr.GetStreamURL(&client, authConfig, episode.Links.Streams.Href, c.String("locale"))
+
+	if err != nil {
+		return err
+	}
+
+	resolutions, err := cr.GetResolutions(&client, authConfig, streamURL, episode)
+
+	fmt.Println()
+	for _, resolution := range resolutions {
+		fmt.Println(resolution)
+	}
+
+	return nil
 }
